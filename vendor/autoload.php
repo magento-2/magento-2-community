2fe166ea52ece9904c08cb3980d7a37d06ad3477
Jimmy <jimmy@mageplaza.com>
2018-09-27 17:10:32 +0700

    Add version 2.2.6

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit4cc426092fca4bd3f88a79ac813ae75c::getLoader();
+return ComposerAutoloaderInit4dbd6d03a2e4327b3d257d87f16e3425::getLoader();
